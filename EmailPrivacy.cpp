52cc7cce6b16d62d0ed1a453a44b94bf08d9c805
Shubham.Prasad <70384931+Shu6hamPrasad@users.noreply.github.com>
2021-06-26 20:34:49 +0530

    Create EmailPrivacy.cpp
    it hides your email address

---
+++ EmailPrivacy.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+string macOS(string email)
+{
+    int length;
+    string finalAns = "";
+    for (int i = 0; i < email.length(); i++)
+    {
+        if (email.at(i) == '@')
+            length = i;
+    }
+    if (length < email.length())
+    {
+        string str = "";
+        for (int l = 3; l < length; l++)
+        {
+            str = str + "*";
+        }
+        email.replace(3, (length - 3), str);
+        finalAns = email;
+    }
+    else
+    {
+        finalAns = "Sorry, Invalid String Found!";
+    }
+    return finalAns;
+}
+int main()
+{
+    string str;
+    cin >> str;
+    cout << macOS(str) << endl;
+    return 0;
+}
